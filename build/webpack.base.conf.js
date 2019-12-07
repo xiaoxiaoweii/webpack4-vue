fa3b69ca5ef6b77c2d9fdb9ca6fdfc784bfd8624
“晓晓伟” <2679656468@qq.com>
2019-12-07 17:43:44 +0800

    autoprefixer

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -43,7 +43,7 @@ module.exports = {
         },
         {
           test: /\.css$/,
-          use: ['vue-style-loader', 'css-loader']
+          use: ['vue-style-loader', 'css-loader', 'postcss-loader']
         }
     ]
   },
